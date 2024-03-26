e0452fd207d12007952330991d4efcd1d77897b1
jesper Barthelsson <JEsperbarthelsson@gmail.com>
2024-03-26 14:21:32 +0000

    Fix problem in frontend app

--- music_app/frontend/urls.py
+++ music_app/frontend/urls.py
@@ -4,9 +4,9 @@ from .views import index
 app_name = 'frontend'
 
 urlpatterns = [
-    path('', index, name=''),
-    path('info', index),
-    path('join', index),
-    path('create', index),
-    path('room/<str:roomCode>', index)
+    path('', index, name='index'),
+    path('info/', index), 
+    path('join/', index), 
+    path('create/', index), 
+    path('room/<str:roomCode>/', index),  
 ]
